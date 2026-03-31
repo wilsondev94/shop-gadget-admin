a673adc105840f41f353df6b75775e27c5be1658
devwilson <ohioleomole@gmail.com>
2026-03-31 18:19:59 +0000

    add header and footer

---
+++ src/components/render-mounted.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import React, { ReactNode, useEffect, useState } from "react";
+
+// To avoid hydration errors
+export const RenderMounted = ({ children }: { children: ReactNode }) => {
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => setMounted(true), []);
+
+  if (!mounted) return null;
+
+  return <>{children}</>;
+};
